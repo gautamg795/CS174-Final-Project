fe73d9860147cb38e810a28b421bdfc96c9eeb64
Gautam Gupta <blerb795@gmail.com>
2015-05-17 04:08:15 -0700

    more initialization code

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -22,14 +22,54 @@ function initAllShaders() {
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "modelView");
 }
 
+/**
+ * Creates the vertex, normal, and texCoord buffers for each mesh.
+ * Binds it and everything, so we don't have to do any of that ourselves
+ */
 function initBuffers() {
-
+    // Initialize the mesh buffers,
+    // then set app.models to contain the mesh
+    // app.models will also contain other model data
+    for (mesh in app.meshes) {
+        OBJ.initMeshBuffers(gl, app.meshes[mesh]);
+        app.models[mesh] = {};
+        app.models[mesh].mesh = app.meshes[mesh];
+    }
 }
 
-function initPlanet() {
-
-}
-
+/**
+ * Load the textures for the models and store them in app.models.*
+ */
 function initTextures() {
+    // initTexture(app.models.spaceship, "assets/textures/some_texture.jpg");
+    // initTexture(app.models.skybox, "assets/textures/some_texture.jpg");
+    // initTexture(app.models.planet, "assets/textures/some_texture.jpg");
+}
 
+/**
+ * Helper function for initTextures
+ * Called once for each model
+ * @param  {Model} object The model being textured
+ * @param  {String} path   Relative path to the texture image
+ */
+function initTexture(object, path) {
+    object.texture = gl.createTexture();
+    object.texture.image = new Image();
+    object.texture.image.crossOrigin = "anonymous";
+    object.texture.image.onload = function() {
+         handleLoadedTexture(object.texture);
+    }
+    object.texture.image.src = path;
+}
+
+function handleLoadedTexture(texture) {
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.bindTexture(TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    // Good practice to leave the active texture unbound
+    gl.bindTexture(gl.TEXTURE_2D, null);
 }
