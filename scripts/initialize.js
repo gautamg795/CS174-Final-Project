bb84bd8decc783c88f7837b4bd2061f9d445c608
Gautam Gupta <blerb795@gmail.com>
2015-05-17 03:23:14 -0700

    more shader goodness

--- scripts/initialize.js
+++ scripts/initialize.js
@@ -3,8 +3,23 @@ function initGL(c) {
     gl.viewport(0, 0, c.width, c.height);
 }
 
+/**
+ * Gets the shaders from the HTML and compiles them into @var [shaderProgram]
+ * Sets up the vertex attribute arrays and uniforms, storing their locations into the program variable
+ */
 function initAllShaders() {
+	shaderProgram = initShaders(gl, "vertex-shader", "fragment-shader");
+	gl.useProgram(shaderProgram);
 
+	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vPosition");
+	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+	shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "vNormal");
+	gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+	shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "vTexCoord");
+	gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+
+	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "projection");
+	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "modelView");
 }
 
 function initBuffers() {
