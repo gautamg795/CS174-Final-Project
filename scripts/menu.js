b7a19d8e8010d9df404b0fdc3d8b040ad5ed1068
Gautam Gupta <blerb795@gmail.com>
2015-05-17 03:35:00 -0700

    add JS functions to update UI

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
-	$('#start-button').click(function() {
-		$('#gl-canvas').css('display', 'block');
-		$('#hud').css('display', 'block');
-		$('#menu').hide();
-		// initialize webgl
-	});
+    $('#start-button').click(function() {
+        $('#gl-canvas').css('display', 'block');
+        $('#hud').css('display', 'block');
+        $('#menu').hide();
+        // initialize webgl
+    });
 });
+
+/**
+ * Updates the speedometer UI element
+ * @param {float} val Arbitrary speed from 0 to 180
+ */
+function setSpeed(val) {
+    // clamp val to [0, 180] range
+    val = val < 0 ? 0 : (val > 180 ? 180 : val);
+    $("#speed-needle").css("-webkit-transform", "rotate(" + val + "deg)");
+}
+
+/**
+ * Updates the fuel gauge UI element
+ * @param {float} pct Fuel amount from 0 to 100
+ */
+function setFuel(pct) {
+	// clamp pct to [0, 100] range
+	pct = pct < 0 ? 0 : (pct > 100 ? 100 : pct);
+	$("#fuel-bar").css("-webkit-transform", "scale(1, " + pct/100.0 + ")");
+}
