f4dde20e44d6774d3d86ed7434e183ba065032ff
Gautam Gupta <blerb795@gmail.com>
2015-05-18 19:16:04 -0700

    add inertial dampening with Z key

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -44,6 +44,28 @@ function handleKeysPressed() {
         if (app.keysPressed[88] === true) {
             app.ship.thrust = 0;
         }
+
+        if (app.keysPressed[90] === true) {
+            app.ship.thrust = 0;
+            var finalFuel = app.ship.fuel - length(app.ship.velocity) / 8;
+            $({
+                x: app.ship.velocity[0],
+                y: app.ship.velocity[1],
+                z: app.ship.velocity[2],
+                fuel: app.ship.fuel
+            }).animate({
+                x: 0,
+                y: 0,
+                z: 0,
+                fuel: finalFuel
+            }, {
+                duration: 1500,
+                step: function() {
+                    app.ship.velocity = [this.x, this.y, this.z];
+                    app.ship.fuel = this.fuel;
+                }
+            });
+        }
     } else {
         // Do something for the other game state(s)
     }
