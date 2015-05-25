0adf0525acbb46770ab960455adca19f99d03461
Gautam Gupta <gautamgupta@ucla.edu>
2015-05-24 18:18:30 -0700

    look above the ship

--- scripts/space.js
+++ scripts/space.js
@@ -51,7 +51,7 @@ function drawSpace() {
     var eye = mult(rotate(-app.headingBuffer.pop()+180, [0, 1, 0]), translate(0, 10, 40));
     eye = mult(translate(app.ship.position), eye);
     eye = multMatVec(eye, vec4(0, 0, 0, 1)).slice(0, 3);
-    var viewMatrix = lookAt(eye, app.ship.position, [0, 1, 0]);
+    var viewMatrix = lookAt(eye, vec3(app.ship.position[0], app.ship.position[1]+10, app.ship.position[2]), [0, 1, 0]);
     // Only need the next line if we end up switching shaders
     // gl.useProgram(shaderProgram)
     var mvMatrix = scale(0.05, 0.05, 0.05);
